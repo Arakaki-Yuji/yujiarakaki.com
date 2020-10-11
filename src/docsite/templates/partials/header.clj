8296f2676d2604d15d074f5bb0bb4cecd04f8a4a
Arakaki-Yuji <arakaji0830@gmail.com>
2020-10-11 10:16:38 +0900

    プロフィールページは不要になったので削除する

--- src/docsite/templates/partials/header.clj
+++ src/docsite/templates/partials/header.clj
@@ -3,9 +3,7 @@
 (def header
   [:header
    [:div.headerWrapper
-    [:a {:href "/profile.html"}
-     [:img.brandLogo {:src "/assets/images/icon.jpg"}]
-     ]
+    [:img.brandLogo {:src "/assets/images/icon.jpg"}]
     [:div.brandTitle
      [:a {:href "/"} "Yuji Arakaki"]]
     ]
